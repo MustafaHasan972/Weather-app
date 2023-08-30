bf9ebc63d834f650ed79495c36ad447ad9d011c0
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-08-30 19:18:09 +0530

    Added complete code for weather app

---
+++ app.js
@@ -0,0 +1,54 @@
+const apikey = "a04ea3149546b5193f788de5e9ea2493";
+
+const weatherDataEl = document.getElementById("weatherData"); // Corrected the selector syntax
+
+const cityInputEl = document.getElementById("cityInput"); // Corrected the selector syntax
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityName = cityInputEl.value;
+  getWeatherData(cityName);
+});
+
+async function getWeatherData(cityName) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=metric`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not successful");
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    const temperature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
+      `Humidity: ${data.main.humidity}%`,
+      `Wind Speed: ${data.wind.speed} m/s`,
+    ];
+
+    weatherDataEl.querySelector(
+      ".icon"
+    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+    weatherDataEl.querySelector(
+      ".temperature"
+    ).textContent = `${temperature}°C`;
+    weatherDataEl.querySelector(".description").innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {
+    weatherDataEl.querySelector(".icon").innerHTML = "";
+    weatherDataEl.querySelector(".temperature").textContent = "";
+    weatherDataEl.querySelector(".description").textContent =
+      "An error happened, please try again later";
+    weatherDataEl.querySelector(".details").innerHTML = "";
+  }
+}
