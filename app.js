b552dfe90f3a9fa737923e9d8b30e38472bee583
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-08-31 22:14:35 +0530

    Updated files

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-const apikey = "a04ea3149546b5193f788de5e9ea2493";
+const apikey = "46f80a02ecae410460d59960ded6e1c6";
 
-const weatherDataEl = document.getElementById("weatherData");
+const weatherDataEl = document.getElementById("weather-data");
 
-const cityInputEl = document.getElementById("cityInput");
+const cityInputEl = document.getElementById("city-input");
 
 const formEl = document.querySelector("form");
 
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
-  const cityName = cityInputEl.value;
-  getWeatherData(cityName);
+  const cityValue = cityInputEl.value;
+  getWeatherData(cityValue);
 });
 
-async function getWeatherData(cityName) {
+async function getWeatherData(cityValue) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
     );
 
     if (!response.ok) {
-      throw new Error("Network response was not successful");
+      throw new Error("Network response was not ok");
     }
 
     const data = await response.json();
 
     const temperature = Math.round(data.main.temp);
+
     const description = data.weather[0].description;
+
     const icon = data.weather[0].icon;
+
+    const cityName = data.name;
+    console.log(cityName);
 
     const details = [
       `Feels like: ${Math.round(data.main.feels_like)}°C`,
       `Humidity: ${data.main.humidity}%`,
-      `Wind Speed: ${data.wind.speed} m/s`,
     ];
 
+    weatherDataEl.querySelector(".city").innerHTML = `${cityName}`;
     weatherDataEl.querySelector(
       ".icon"
     ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
@@ -41,15 +46,23 @@ async function getWeatherData(cityName) {
       ".temperature"
     ).textContent = `${temperature}°C`;
     weatherDataEl.querySelector(".description").textContent = description;
-    weatherDataEl.querySelector(".description").innerHTML = details
+
+    weatherDataEl.querySelector(".details").innerHTML = details
       .map((detail) => `<div>${detail}</div>`)
       .join("");
+
+    cityInputEl.value = "";
   } catch (error) {
-    console.log(error);
-    weatherDataEl.querySelector(".icon").innerHTML = "";
-    weatherDataEl.querySelector(".temperature").textContent = "";
-    weatherDataEl.querySelector(".description").textContent =
-      "An error happened, please try again later";
-    weatherDataEl.querySelector(".details").innerHTML = "";
+    if (cityInputEl.value === undefined) {
+      weatherDataEl.querySelector(".description").textContent =
+        "Please enter a city name";
+    } else {
+      weatherDataEl.querySelector(".icon").innerHTML = "";
+      weatherDataEl.querySelector(".temperature").textContent = "";
+      weatherDataEl.querySelector(".description").textContent =
+        "An error happened, please try again later";
+
+      weatherDataEl.querySelector(".details").innerHTML = "";
+    }
   }
 }
